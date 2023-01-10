d75852009857f0dd34490f7e0106b44b2dfb4720
User <6210210299@email.psu.ac.th>
2023-01-10 14:38:59 +0700

    Update login bloc & state

--- lib/src/bloc/login/login_bloc.dart
+++ lib/src/bloc/login/login_bloc.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
+  LoginBloc() : super(LoginState()) {
     on<LoginEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/src/bloc/login/login_state.dart
+++ lib/src/bloc/login/login_state.dart
@@ -1,10 +1,18 @@
 part of 'login_bloc.dart';
 
 @immutable
-abstract class LoginState {
+ class LoginState extends Equatable  {
   final int count; // ห้ามเเทรกเเซงค่าของตัวเเปร เช่น render อยู่เเล้วเปลี่ยนค่าทำให้ค่าผิดเพี้ยนได้
 
-  LoginState({required this.count});
+  const LoginState({this.count = 0 });
+
+  LoginState copyWith(int? count) {  // int? count => คือส่งหรือไม่ส่งข้อความก็ได้
+   //clone new object
+   return LoginState(count: count ?? this.count); // ?? this.count => ถ้าไม่มีการส่งค่าให้ส่งค่าเดิม
+  }// return to LoginState
+  @override
+  // TODO: implement props
+  List<Object?> get props => [count];
 }
 
 
